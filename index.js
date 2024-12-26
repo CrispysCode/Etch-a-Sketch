6357cd6b43baed9dbd21173a1fb5fd024cae8b9d
CrispysCode <pr0ject0dinx@gmail.com>
2024-12-26 01:47:42 -0600

    added a random color func, so when a user hovers each grid item, it changes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const container = document.querySelector(".container");
 
 // grid creation function 
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
@@ -8,8 +16,20 @@ function createGrid(size) {
             
             div.classList.add("gridItem");
 
-            container.appendChild(div);
-        
+            container.appendChild(div);  
     }
+
+const gridItems = document.querySelectorAll(".gridItem");
+
+   gridItems.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+       div.style.backgroundColor = getRandomColor();
+    })
+})
 }
+
+
 createGrid(16);
+
+
+console.log(gridItem);
