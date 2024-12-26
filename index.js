8fee9f32dc00f02a19d5781b9a3b30816ba3e381
CrispysCode <pr0ject0dinx@gmail.com>
2024-12-26 01:25:25 -0600

    created proper styling to render a 16x16 grid

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const container = document.querySelector(".container");
 
 // grid creation function 
 
-function createGrid(rows, cols) {
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
+function createGrid(size) {
+    for (let i = 0; i < size * size; i++) {
             const div = document.createElement("div");
             
             div.classList.add("gridItem");
-            
+
             container.appendChild(div);
-        }
+        
     }
 }
-createGrid(16, 16);
+createGrid(16);
