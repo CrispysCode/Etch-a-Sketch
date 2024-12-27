09f3627909042a183e44bb19e18ecc8a346d9af5
CrispysCode <pr0ject0dinx@gmail.com>
2024-12-27 00:45:48 -0600

    Went a different route, completed project using do while loop

--- index.js
+++ index.js
@@ -1,50 +1,37 @@
 const container = document.querySelector(".container");
-const gridBtn = document.querySelector(".newGrid");
-// grid creation function 
-function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-function createGrid(size) {
-    clearGrid();
-    const itemSize = 100 / size;
-
-    for (let i = 0; i < size * size; i++) {
-            const div = document.createElement("div");
-            
-            div.classList.add("gridItem");
-
-            container.appendChild(div);  
-    }
-
-const gridItems = document.querySelectorAll(".gridItem");
-
-   gridItems.forEach((div) => {
-        div.addEventListener("mouseover", () => {
-       div.style.backgroundColor = getRandomColor();
-    })
-})
-}
-function clearGrid() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-}
-
-function makeGrid() {
-  const userInput = parseInt(prompt("Enter a number to create a grid"), 10);
-  createGrid(userInput);
-}
-gridBtn.addEventListener("click", () => {
-    makeGrid()
-})
-
 createGrid(16);
 
 
-console.log(gridItem);
+
+  function createGrid(size) {
+   
+    for (let i = 0; i < size; i++) {;
+        for (let j = 0; j < size; j++) {
+            const div = document.createElement("div");
+            div.className = "grid-div";
+            div.addEventListener("mouseover", function() {
+                div.classList.add("hover");
+            });
+            div.style.flex = "1 1 " + (100 / size) + "%";
+            container.appendChild(div);
+        }
+    }
+}
+const newButton = document.querySelector("button");
+newButton.addEventListener("click", function(){
+    let newSize;
+    let error = false;
+    do {
+        newSize = prompt("Enter the number of squares per side:");
+        newSize = Number(newSize);
+        if(!Number.isInteger(newSize) || newSize < 2 || newSize > 100) {
+            alert("Please enter a number between 2 and 100.");
+            error = true;
+            continue;
+        }
+
+        error = false;
+        document.querySelectorAll(".grid-div").forEach(el => el.remove());
+        createGrid(newSize);
+    } while(error)
+});
